a94f138dce1c603f895fd5464dbf245e0e060f11
florisweb <floris@florisweb.tk>
2021-08-27 16:58:41 +0200

    Started working on the pairing=animation and styling.

--- home/src/js/mainContent/pairMenu.js
+++ home/src/js/mainContent/pairMenu.js
@@ -3,53 +3,76 @@ import '../../css/mainContent/pairMenu.css';
 import React from 'react';
 import Server from '../server';
 import SideBar from '../sideBar';
+import icon from '../../images/icons/plantIcon.png';
 
 
 let renderDeviceElements;
-let setBindState;
-let setOpenState;
+let setPairListOpenState;
+let setPairMenuOpenState;
+
+let setCurDevice;
+
 const PairMenu = new function() {
   this.render = function() {
-    return <PairMenuElement/>
+    return <div>
+      <PairMenuElement/>
+      <PairMenuListElement/>
+    </div>
   }
 
   this.open = async function() {
+    setPairListOpenState(true);
     await this.update();
-    setOpenState(true);
   }
   this.close = function() {
-    setOpenState(false);
+    setPairListOpenState(false);
+    setPairMenuOpenState(false);
   }
 
 
   this.update = async function() {
     let deviceElements = [];
     let devices = await Server.getUnboundDevices();
+    
+    devices.sort(function (a, b) {
+      return a.onSameNetwork < b.onSameNetwork;
+    });
+
+    if (devices[0] && devices.length == 1)
+    {
+      setCurDevice(devices[0]);
+      setPairMenuOpenState(true);
+      setPairListOpenState(false);
+    }
     for (let device of devices) deviceElements.push(<DeviceElement data={device}/>);
     renderDeviceElements(deviceElements);
   }
 }
 
-function PairMenuElement() {
-  let binding;
-  let openState;
-  [binding, setBindState] = React.useState(false);
-  [openState, setOpenState] = React.useState(false);
 
-  let deviceElements;
-  [deviceElements, renderDeviceElements] = React.useState('Searching for devices...');
+function PairMenuElement() {
+  let curDevice;
+  [curDevice, setCurDevice] = React.useState({name: 'nameless', id: false});
+  let [binding, setBindState] = React.useState(false);
+  
+  let openState;
+  [openState, setPairMenuOpenState] = React.useState(false);
 
   return (
-    <div className={'popup pairMenu' + (openState ? '' : ' hide') + (binding ? ' binding' : '')}> 
-      {deviceElements}
+    <div className={'popup pairMenu pairSingleDevice ' + (openState ? '' : ' hide') + (binding ? ' binding' : '')}> 
+      <div className='text header'>{curDevice.name}</div>
+      <img src={icon} className='icon deviceIcon'/>
+      <div className='text button bBoxy bindButton' onClick={bindDevice}>Connect</div>
+      <div className='text button bBoxy' onClick={() => {
+         setPairMenuOpenState(false);
+         setPairListOpenState(true);
+      }}>Other devices...</div>
     </div>
   );
-}
 
-function DeviceElement({data}) {
-  return <div className='deviceOption' onClick={() => {
+  function bindDevice() {
     setBindState(true);
-    Server.bindDevice(data.id).then(async function(_response) {
+    Server.bindDevice(curDevice.id).then(async function(_response) {
       setBindState(false);
 
       if (!_response.result) return;
@@ -57,8 +80,32 @@ function DeviceElement({data}) {
     
       await SideBar.deviceList.update();
     });
+  }
+}
+
+
+function PairMenuListElement() {
+  let openState;
+  [openState, setPairListOpenState] = React.useState(false);
+
+  let deviceElements;
+  [deviceElements, renderDeviceElements] = React.useState('Searching for devices...');
+
+  return (
+    <div className={'popup pairMenu' + (openState ? '' : ' hide')}> 
+      {deviceElements}
+    </div>
+  );
+}
+
+function DeviceElement({data}) {
+  console.log(data);
+  return <div className='deviceOption' onClick={() => {
+    setCurDevice(data);
+    setPairListOpenState(false);
+    setPairMenuOpenState(true);
   }}>
-    {data.name}
+    <div className='text'>{data.name}</div>
     {data.onSameNetwork ? <div className='onSameNetworkIndicator'>Local</div> : ''}
   </div>
 }
